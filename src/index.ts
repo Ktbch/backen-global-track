73902f863eff453f9509d455a64700f2434d57bf
kay2-dev <odufotekayode10@gmail.com>
2026-01-07 18:21:47 +0100

    first push

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+// src/index.ts
+import express from "express";
+import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => res.send("Backend is live"));
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log(`Server running on port ${ process.env.PORT || 3000 }`);
+});
