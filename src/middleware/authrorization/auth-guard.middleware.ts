f8fa6c30628f18c1bc4964334dc18f3bb60b3519
kay2-dev <odufotekayode4@gmail.com>
2026-01-10 20:57:40 +0100

    auth-guard.middlware created

---
+++ src/middleware/authrorization/auth-guard.middleware.ts
@@ -0,0 +1,28 @@
+import { NextFunction, Request, Response } from "express";
+import { UnAuthorisedRequestError } from "../../utils/app-error";
+import jwt, { JwtPayload } from 'jsonwebtoken'
+import AppConfig from "../../config.ts/config";
+
+
+declare module 'express-serve-static-core' {
+    interface Request {
+        user?: JwtPayload;
+    }
+}
+
+
+
+export function authGuard (req: Request, res: Response, next: NextFunction) {
+    const token = req.cookies.accessToken
+
+    if (!token) throw new UnAuthorisedRequestError()
+
+    try
+    {
+        const decoded = jwt.verify(token, AppConfig.JWT_SECRET)
+        req.user = decoded as JwtPayload
+    } catch (error)
+    {
+
+    }
+}
