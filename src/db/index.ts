ed76f3acfd30e83ef7ca33959413620353c6b3b7
kay2-dev <odufotekayode4@gmail.com>
2026-01-17 13:14:45 +0100

    ignoring src

--- src/db/index.ts
+++
@@ -1,6 +0,0 @@
-// db.index.ts
-import 'dotenv/config';
-import { drizzle } from "drizzle-orm/mysql2";
-// import AppConfig from '../config.ts/config';
-
-export const db = drizzle(process.env.DATABASE_URL!);
--- src/utils/extract-session-meta.ts
+++
@@ -1,9 +0,0 @@
-import { getClientIp } from "./get-client-ip"
-import { Request } from "express"
-
-export const extractSessionMeta = (req: Request) => {
-    return {
-        ip: getClientIp(req),
-        userAgent: req.headers[ "user-agent" ] || null,
-    }
-}
